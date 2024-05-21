069dd735905d4229d6d8910b80ebd77e44b9d7f7
Bushra <bshra.m@sybertechnology.com>
2024-05-21 14:30:13 +0200

    Added Onboarding screen

--- app/src/main/java/com/bush/littlelemoncappro/ui/theme/Color.kt
+++ app/src/main/java/com/bush/littlelemoncappro/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package com.bush.littlelemoncappro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+object LittleLemonColors {
+    val green = Color(0xff495e57)
+    val cloud = Color(0xffedefee)
+    val yellow = Color(0xfff4ce14)
+    val charcoal = Color(0xff333333)
+}
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
